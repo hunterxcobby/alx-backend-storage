846503db2f1d71b419593f5b3e55b54b83c9ecb8
hunterxcobby <solomonsefah13@gmail.com>
2024-05-17 10:52:31 +0000

    feat: Add function to list all documents in a collection

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""Module that lists all documents in a collection.
+"""
+
+
+def list_all(mongo_collection):
+    """
+    Lists all documents in a collection.
+
+    Args:
+        mongo_collection: pymongo collection object.
+
+    Returns:
+        A list of documents in the collection.
+        Returns an empty list if no documents exist.
+    """
+    doc_list = []
+
+    for doc in mongo_collection.find():
+        if doc:
+            doc_list.append(doc)
+    return doc_list
